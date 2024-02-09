aa538e86402e852a06346be6bd4e4dac974708fa
ren <0806renkun@gmail.com>
2024-02-09 15:45:11 +0900

    template

---
+++ src/client/java/whistlemodfabric/WhistleModFabricClient.java
@@ -0,0 +1,10 @@
+package whistlemodfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WhistleModFabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
